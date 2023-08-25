fd9d92966b889fea39dac443b08a7c09fbb235ba
VitaliySaburdo <vsaburdo@gmail.com>
2023-08-25 20:05:43 +0300

    add files

---
+++ ground.js
@@ -0,0 +1,37 @@
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
+
+export const ground = (() => {
+  class Ground {
+    constructor(params) {
+        this.params = params;
+        this.load();
+    }
+
+    load = () => {
+      const loader = new FBXLoader();
+
+      loader.load("Brain.glb", (glb) => {
+        glb.cast.shadow = false;
+        glb.receiveShadow = true;
+        glb.position.y = -3;
+        glb.rotation.x = -Math.PI / 1;
+        glb.rotation.y = -Math.PI / 2;
+
+        glb.scale.setScalar(0.09);
+
+        this.mesh = glb;
+        this.params.scene.add(glb);
+      });
+    };
+
+      Update(timeElapsed) {
+          if (this.mesh.position.x > -100) {
+              this.mesh.position.x -= 1; 
+          } else {
+             this.mesh.position.x = 1;  
+          }
+      }
+      
+    };
+    return Ground
+})();
--- index.js
+++ index.js
@@ -1,81 +1,99 @@
-import * as THREE from 'https://unpkg.com/three/build/three.module.js'
+import * as THREE from "https://unpkg.com/three/build/three.module.js";
 
-class MainWorld{
-    constructor() {
-        this.gameStarted = false;
-        document.getElementById('game-menu').onclick = () => {
-            this.onStart
+import { ground } from "./ground";
+
+class MainWorld {
+  constructor() {
+    this.gameStarted = false;
+    document.getElementById("game-menu").onclick = () => {
+      this.onStart();
+    };
+  }
+  onStart() {
+    document.getElementById("game-menu").style.display = "none";
+    document.getElementById("game-menu1").style.display = "none";
+    this.gameStarted = true;
+  }
+  initialize() {
+    this.threejs = THREE.WebGLRenderer({
+      antialias: true,
+    });
+    this.threejs.outputEncoding = THREE.sRGBEncoding;
+    this.threejs.gammaFactor = 2.2;
+    this.threejs.shadow.enabled = true;
+
+    this.threejs.setPixelRatio(window.devicePixelRatio);
+    this.threejs.setResize(window.innerWidth, window.innerHeight);
+
+    document.getElementById("container").appendChild(this.threejs.domElement);
+
+    window.addEventListener(
+      "resize",
+      () => {
+        this.onWindowResize();
+      },
+      false
+    );
+
+    const fov = 60;
+    const aspect = 1920 / 1080;
+    const near = 1;
+    const far = 20000;
+
+    this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this.camera.position.set(-15, 7, 10);
+    this.camera.lookAt(120, -20, 10);
+
+    this.scene = new THREE.Scene();
+
+    const color = 0xffffff;
+    const intercity = 0.6;
+    const light = new THREE.AmbientLight(color, intercity);
+    light.position.x = 1200;
+    light.position.y = 3;
+
+    this.scene.add(light);
+
+    this.scene.background = new THREE.Color(0x000000);
+    this.scene.fog = new THREE.Color(0x000000, 0.00125);
+
+    this.ground = ground.Ground({ scene: this.scene });
+
+    this.gameOver = false;
+    this.previousRAF = null;
+    this.RAF();
+
+    this.onWindowResize();
+  }
+
+  onWindowResize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.threejs.setResize(window.innerWidth, window.innerHeight);
+  }
+
+  RAF() {
+    requestAnimationFrame((t) => {
+      if (this.previousRAF === null) {
+        this.previousRAF = t;
+      }
+    });
+
+    this.RAF();
+    this.Step((t - this.previousRAF) / 1000);
+    this.threejs.render(this.scene, this.camera);
+    this.previousRAF = t;
+    }
+    
+    Step(timeElapsed) {
+        if (this.gameStarted || this.gameOver) {
+            return
         }
     }
-    onStart() {
-        document.getElementById('game-menu').style.display = 'none';
-        document.getElementById('game-menu1').style.display = 'none';
-        this.gameStarted = true;
-
-    }
-    initialize() {
-        this.threejs = THREE.WebGLRenderer({
-            antialias: true,
-        })
-        this.threejs.outputEncoding = THREE.sRGBEncoding;
-        this.threejs.gammaFactor = 2.2;
-        this.threejs.shadow.enabled = true;
-
-        this.threejs.setPixelRatio(window.devicePixelRatio);
-        this.threejs.setResize(window.innerWidth, window.innerHeight);
-
-        document.getElementById('container').appendChild(this.threejs.domElement);
-
-        window.addEventListener('resize', () => {
-            this.onWindowResize()
-        }, false);
-
-        const fov = 60;
-        const aspect = 1920 / 1080;
-        const near = 1;
-        const far = 20000;
-
-        this.camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        this.camera.position.set(-15, 7, 10);
-        this.camera.lookAt(120, -20, 10);
-
-        this.scene = new THREE.Scene();
-
-        const color = 0xFFFFFF;
-        const intercity = 0.6;
-        const light = new THREE.AmbientLight(color, intercity);
-        light.position.x = 1200;
-        light.position.y = 3;
-
-        this.scene.add(light);
-
-        this.scene.background = new THREE.Color(0x000000);
-        this.scene.fog = new THREE.Color(0x000000, 0.00125);
-
-        this.gameOver = false;
-        this.previousRAF = null;
-
-        this.onWindowResize()
-    }
-
-    onWindowResize() {
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-        this.threejs.setResize(window.innerWidth, window.innerHeight);
-    }
-
-    RAF() {
-        requestAnimationFrame((t) => {
-            if (this.previousRAF === null) {
-                this.previousRAF = t
-            }
-        });
-        this.RAF();
-        this.step((t - this.previousRAF) / 1000);
-        this.previousRAF = t
-    };
 }
 
 let APP = null;
 
-window.addEventListener()
+window.addEventListener("DOMContentLoaded", () => {
+  APP = new MainWorld();
+});
